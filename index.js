3c67bd34bd54d394eb815926e5603a901b83d82f
MineWorldServ <153809468+MineWorldServ@users.noreply.github.com>
2024-03-03 22:25:15 +0500

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Играет в mc.mineworld.pw"];
 
 
 let currentIndex = 0;
